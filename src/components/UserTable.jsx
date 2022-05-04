b2635cab99f01062aeab8aaa7d6b377ac7261ccd
granadalook <tiven17.jsga@gmail.com>
2022-05-04 13:07:06 -0500

    CREACION: Se crea componente UserTable.jsx para  mostrar los usuarios en una tabla  con sus botones de editar y eliminar

---
+++ src/components/UserTable.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+const UserTable = (props) => {
+  console.log(props.users);
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Nombre</th>
+          <th>Usuarios</th>
+          <th>Acciones </th>
+        </tr>
+      </thead>
+      <tbody>
+        {props.users.length > 0 ? (
+          props.users.map((user) => (
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.username}</td>
+              <td>
+                <button
+                  className="button muted-button"
+                  onClick={() => props.editRow(user)}
+                >
+                  Editar
+                </button>
+                <button
+                  className="button muted-button"
+                  onClick={() => props.deleteUser(user.id)}
+                >
+                  Delete
+                </button>
+              </td>
+            </tr>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={3}>No hay usuarios</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default UserTable;
